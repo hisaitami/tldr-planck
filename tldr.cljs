0c2c12cabaef28487484998b674ea43d94a37cc9
hisaitami <hisaitami@gmail.com>
2024-06-08 21:35:35 +0900

    Version 0.7.5
    client specification v2.1

--- tldr.cljs
+++ tldr.cljs
@@ -187,7 +187,7 @@
                   ["-l" "--list" "list all entries in the local database"]
                   [nil, "--random" "show a random command"]])
 
-(def version "tldr.cljs v0.7.4 (spec v2.0)")
+(def version "tldr.cljs v0.7.5 (spec v2.1)")
 
 (defn usage [options-summary]
   (->> ["usage: tldr.cljs [OPTION]... PAGE\n"
